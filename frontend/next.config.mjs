353f64b67e51b1939519ff1c6184489ddacc1bca
Tomasz Ż. <tzuber95@gmail.com>
2025-01-03 16:48:27 +0100

    edit protocols

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '5000',
+                protocol: 'https',
+                hostname: 'nextpc',
+                port: '10000',
                 pathname: '/**',
             },
         ],
