9e785fe2dfa0cc41852dec8d42fbe55d458f9efe
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 13:27:45 +0100

    some changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
 
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'nextpc',
+                port: '10000',
+                pathname: '/**',
+            },
+        ],
+    },
+}
+
+export default nextConfig
