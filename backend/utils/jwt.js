53f5f7c65df0d17bf8f1682e802b3f86fb7a7ef2
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 15:22:31 +0100

    edit token validation

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -18,6 +18,7 @@ const attachCookiesToResponse = ({ res, user }) => {
 		expires: new Date(Date.now() + oneDay),
 		secure: process.env.NODE_ENV === 'production',
 		signed: true,
+		sameSite: 'None',
 	})
 }
 
