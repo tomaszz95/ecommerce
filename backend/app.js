2db01c9ef63aa34c54f7dd0a72f05a7079b68419
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 13:56:42 +0100

    improve cors

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(helmet())
 app.use(mongoSanitize())
 app.use(
 	cors({
-		origin: 'https://nextpc.vercel.app/',
+		origin: 'https://nextpc.vercel.app',
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
