eb96c3d773a7e818b6dbef8b71a4d00472c9b453
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 14:46:13 +0100

    Create registration page

--- frontend/src/components/carousels/photoCarousel/SinglePhoto.tsx
+++ frontend/src/components/carousels/photoCarousel/SinglePhoto.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import FavoriteButton from '../../productPage/productInfo/favorite/FavoriteButton'
 
 import { API_URL } from '../../../constans/url'
@@ -15,13 +17,14 @@ const SinglePhoto = ({ photo, productName, productId, uniqueId }: ComponentType)
     return (
         <div className={styles.carouselItem}>
             <FavoriteButton productId={productId} uniqueId={uniqueId} />
-            <img
+            <Image
                 className={styles.photo}
                 src={`${API_URL}/photos/${photo}`}
                 width={1000}
                 height={1000}
                 alt={`${productName} photo`}
                 crossOrigin="anonymous"
+                unoptimized
             />
         </div>
     )
