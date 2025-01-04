84b9f0ddacf0a346edd094a1ac67a3fefd443de1
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 13:25:55 +0100

    fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true }
 
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'nextpc',
-                port: '10000',
-                pathname: '/**',
-            },
-        ],
-    },
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.(jpg|jpeg|png|gif|bmp|svg)$/i,
-            type: 'asset/resource',
-            generator: {
-                filename: 'static/media/[name].[hash][ext]',
-            },
-        })
-        return config
-    },
-}
-
-export default nextConfig
+module.exports = nextConfig
