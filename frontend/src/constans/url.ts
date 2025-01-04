96045ba3f605097ad875ebe9706224baf37ca02b
Tomasz Ż. <tzuber95@gmail.com>
2025-01-04 13:16:06 +0100

    Fix add order component

--- frontend/src/constans/url.ts
+++ frontend/src/constans/url.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://nextpc.onrender.com/'
-export const FRONTEND_URL = 'https://nextpc.vercel.app/'
+export const API_URL = 'https://nextpc.onrender.com'
+export const FRONTEND_URL = 'https://nextpc.vercel.app'
 // export const API_URL = 'http://localhost:5000'
 // export const FRONTEND_URL = 'http://localhost:3000'
